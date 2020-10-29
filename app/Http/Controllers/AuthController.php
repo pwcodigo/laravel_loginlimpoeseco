e6822c7c6d7f117dc390e49cc4c94ed45387a78c
pwcodigo <pwcodigo@gmail.com>
2020-10-29 14:31:23 -0300

    feat: criando pasta adm e arquivos dashboard e login. Tambem adicionado tailwindcss

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function dashboard(){
+        return view ('dashboard');
+    }
+
+    public function login(){
+        return view('adm.login');
+    }
 }
